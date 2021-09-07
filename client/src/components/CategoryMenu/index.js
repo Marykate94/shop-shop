3f14d2926c126121d7db23de01560fb9fbccdb03
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-09-06 20:57:34 -0400

    feature/global-store

---
+++ client/src/components/CategoryMenu/index.js
@@ -0,0 +1,45 @@
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from '../../utils/GlobalState';
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+
+function CategoryMenu({ setCategory }) {
+
+
+  const [state, dispatch] = useStoreContext();
+
+  const { categories } = state;
+
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  useEffect(() => {
+    // if category data exists or has changed from the response of useQuery the run dispatch
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]
+  );
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            setCategory(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
